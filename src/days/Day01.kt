7a02cde449be105e85f33b5b97b9f2f95b8ec2e0
Nick <nickkuiper44@gmail.com>
2023-12-01 19:00:47 +0100

    Day 1 part 2 Currently not working :)

--- src/days/Day01.kt
+++ src/days/Day01.kt
@@ -12,7 +12,24 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = hashMapOf(
+            "zero" to 0,
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eighth" to 8,
+            "nine" to 9
+        )
+        return input.sumOf { row ->
+            var item = row
+            numbers.forEach { (key, value) ->
+                item = item.replace(key, value.toString())
+            }
+        }
     }
 
     // test if implementation meets criteria from the description, like:
